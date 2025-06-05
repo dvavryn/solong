19649b24212524549c618f43d3d5ad423418bc72
Dominic Vavryn <dvavryn@c3r3p5.42vienna.com>
2025-06-05 16:50:54 +0200

    changes

---
+++ solong.h
@@ -0,0 +1,25 @@
+#ifndef SOLONG_H
+# define SOLONG_H
+
+// open //
+# include <fcntl.h>
+
+
+
+// malloc // free //
+# include <stdlib.h>
+// perror
+# include <stdio.h>
+// strerror
+# include <string.h>
+// exit // close // read // write //
+# include <unistd.h>
+// math
+# include <math.h>
+// mlx
+# include <mlx.h>
+// libft
+# include "../libft/libft.h"
+
+
+#endif
